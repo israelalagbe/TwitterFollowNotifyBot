72cba45a46cca867efb52d61e289d699d2023d64
Israel Alagbe <israelalagbe53@gmail.com>
2021-05-16 20:33:09 +0100

    Updated view files

--- routes/index.js
+++ routes/index.js
@@ -19,44 +19,24 @@ var router = express.Router();
 
 
 /* GET home page. */
-router.get('/', async (req, res, next) => {
+// router.get('/', async (req, res, next) => {
 
-  //   console.time('debug')
-  //   const count = 1000000
-  //   const arrayItems = new Array(count).fill("jdjdjdjdjdjdj").join('')
-  //   const arrayItems2 = new Array(count).fill("jdjdjdjdjdjdj").join('')
+//   //   console.time('debug')
+//   //   const count = 1000000
+//   //   const arrayItems = new Array(count).fill("jdjdjdjdjdjdj").join('')
+//   //   const arrayItems2 = new Array(count).fill("jdjdjdjdjdjdj").join('')
 
-  //   if(md5(arrayItems) === md5(arrayItems2)){
-  //     console.log("yes way", count);
-  //   }
-  //   console.timeEnd('debug')
+//   //   if(md5(arrayItems) === md5(arrayItems2)){
+//   //     console.log("yes way", count);
+//   //   }
+//   //   console.timeEnd('debug')
 
-  //   const b = arrayItems.length * 2;
-  // const kb = (b / 1024).toFixed(2);
+//   //   const b = arrayItems.length * 2;
+//   // const kb = (b / 1024).toFixed(2);
 
-  // console.log(`${kb}KB`);
-
-
-
-  // const response = await Bot.tweet("Second Bot test")
-  // logger.info("Item",response)
-  // console.log(await Bot.getFollowers({user_id: 3062433100}))
-  //   const config = {
-  //     consumer_key: process.env.consumer_key,
-  //     consumer_secret: process.env.consumer_secret,
-  //     access_token: process.env.access_token,
-  //     access_token_secret: process.env.access_token_secret,
-  //     timeout_ms: 60 * 1000, // optional HTTP request timeout to apply to all requests.
-  // }
-  //   const twit = new Twit(config);
-  //   twit.get('https://api.twitter.com/oauth/request_token',(err, response) => {logger.info("Here", err, response)})
-  // await User.create({
-  //   username: 'israelalagbe'
-  // })
-  // const user = await User.findOne()
-  // console.log({user})
-  res.render('index');
-});
+//   // console.log(`${kb}KB`);
+//   res.render('index');
+// });
 
 router.post('/subscribe', async (req, res, next) => {
 
@@ -81,7 +61,7 @@ router.get('/success', async (req, res, next) => {
     const userAccessToken = await Bot.getAccessToken(query)
     const twitterUser = await Bot.getUserCredentials(userAccessToken)
     addUserDetails(userAccessToken, twitterUser)
-    res.send("<h1>Congratulations, you have successfully subscribed</h1>")
+    res.render('success');
     
   } catch (e) {
     logger.error(e)
