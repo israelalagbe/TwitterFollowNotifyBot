396fd729b975d76180df2055dfdb121375e67642
Israel Alagbe <israelalagbe53@gmail.com>
2021-05-17 14:19:18 +0100

    Fixed email required error

--- models/user.js
+++ models/user.js
@@ -12,8 +12,7 @@ const userSchema = new mongoose.Schema({
         required: true,
     },
     email: {
-        type: String,
-        required: true,
+        type: String
     },
     twitter_user_id: {
         type: String,
