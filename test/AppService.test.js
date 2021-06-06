a13097311129ceb0b4f793fdc56fd4490e95f3e3
Israel Alagbe <israelalagbe53@gmail.com>
2021-06-06 20:01:36 +0100

    Added test for analyzeSubscribersFollowers function

--- test/AppService.test.js
+++ test/AppService.test.js
@@ -3,12 +3,15 @@ const AppService = require('../helpers/AppService');
 const Bot = require('../helpers/Bot');
 const findUnfollowers = require('../helpers/findUnfollowers');
 const humanizeArray = require('../helpers/humanizeArray');
-const user = require('../models/user');
+const pause = require('../helpers/pause');
+const User = require('../models/user');
 
+jest.mock('../helpers/pause');
+jest.mock('../models/user', () => jest.fn());
 jest.mock('../helpers/humanizeArray', () => jest.fn());
 jest.mock('../helpers/findUnfollowers', () => jest.fn());
 jest.mock('../helpers/Bot', () => jest.fn());
-jest.mock("../config/logger", () => ({info: jest.fn()}));
+jest.mock("../config/logger", () => ({info: jest.fn(), error: jest.fn()}));
 
 describe('AppService Test', () => {
     beforeEach(() => {
@@ -75,12 +78,29 @@ describe('AppService Test', () => {
         expect(sendDirectMessageMock).toHaveBeenCalledWith(user.twitter_user_id, message)
     })
 
-    it('analyzeSubscriber test', async () => {
-        jest.spyOn(Bot, 'getFollowers').mockReturnValueOnce(Promise.resolve({
-            ids: ['1', '2', '3'],
-            next_cursor_str: '4'
-          }))
-    })
-
     
+
+    it('analyzeSubscribersFollowers test', async () => {
+        const users = [{_id: 1}, {_id: 2}, {_id: 3}]
+        // @ts-ignore
+        User.find = jest.fn(() => {
+            return Promise.resolve(users)
+        });
+        jest.spyOn(AppService, 'analyzeSubscriber').mockReturnValueOnce(Promise.resolve())
+
+        const runningTime = 1;
+
+        jest.spyOn(Date, 'now').mockReturnValueOnce(0).mockReturnValueOnce(runningTime);
+
+        await AppService.analyzeSubscribersFollowers()
+        expect(AppService.analyzeSubscriber).toHaveBeenCalledTimes(3)
+        expect(AppService.analyzeSubscriber).toHaveBeenCalledWith({_id: 1})
+        expect(AppService.analyzeSubscriber).toHaveBeenCalledWith({_id: 2})
+        expect(AppService.analyzeSubscriber).toHaveBeenCalledWith({_id: 3})
+
+        expect(pause).toHaveBeenCalledTimes(1)
+        const breakTime = 1000 * 60 * 60 * 2
+        expect(pause).toHaveBeenCalledWith(breakTime - runningTime)
+
+    });
 });
